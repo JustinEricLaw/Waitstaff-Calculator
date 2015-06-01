d073287b57bcaee34f9afb02f0b0452c0ef3abf3
J Law <justin.eric.law@gmail.com>
2015-06-01 17:59:33 -0400

    Update JS, add controller, gender options.

---
+++ min/myApp-min.js
@@ -0,0 +1,1 @@
+angular.module("myApp",[]).controller("myController",function(e){e.celebrity="Celebrity",e.noun="Noun",e.objects="Objects",e.adjective="Adjective",e.noun2="Noun 2",e.beverage="Type of Beverage",e.dogname="Name",e.place="Place",e.liquid="Type of Liquid",e.gender={type:"female",her_his:"her",she_he:"she",her_him:"her"}});
