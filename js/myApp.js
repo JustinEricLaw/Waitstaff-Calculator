6cd623f00e983dc46bbcc326f0441a9b359db8ba
J Law <justin.eric.law@gmail.com>
2015-06-08 01:40:50 -0400

    Add ng functionality to earnings

--- js/myApp.js
+++ js/myApp.js
@@ -1,42 +1,35 @@
 angular.module('myApp', [])
   .controller('myController', function($scope){
 
+    $scope.meal = {};
+    $scope.customer = {};
+    $scope.earnings = {};
+
     $scope.submit = function() {
       if($scope.myForm.$valid) {
-        console.log('The Form is Valid');
-        compute();
-      } else {
-        console.log('The Form is Not Valid');
+        
+        // Customer Charges
+        $scope.customer.subtotal = $scope.meal.price + ($scope.meal.price * ($scope.meal.tax/100));
+        $scope.customer.tip = $scope.customer.subtotal * ($scope.meal.tip/100);
+        $scope.customer.total = $scope.customer.subtotal + $scope.customer.tip;
+
+        // Earnings Info
+        // Why do we need the || 0 and || 1 ??
+        $scope.earnings.total = $scope.customer.tip + ($scope.earnings.total || 0);
+        $scope.earnings.count = ++$scope.earnings.count || 1;
+        $scope.earnings.avg = $scope.earnings.total / $scope.earnings.count;
+
       }
-    };
-
-    // Why do I need to do this?
-    $scope.customer_subtotal = 0;
-    $scope.customer_tip = 0;
-    $scope.customer_total = 0;
-
-
-    var compute = function () {
-
-      // Customer Charges
-      $scope.customer_subtotal = function () {
-        return $scope.meal_price + ($scope.meal_price * $scope.meal_tax/100);
-      };
-      $scope.customer_tip = function () {
-        return $scope.customer_subtotal() * ($scope.meal_tip/100);
-      };
-      $scope.customer_total = function () {
-        return $scope.customer_subtotal() + $scope.customer_tip();
-      };
-
-      // Earnings Info
-        $scope.tip_total += $scope.customer_tip();
-        $scope.meal_count++;
-        $scope.avg_tip = $scope.tip_total / $scope.meal_count;
     };
 
 
     // Reset All Data
+    $scope.resetAll = function () {
+      $scope.myForm.$setPristine();
 
+      $scope.meal = {};
+      $scope.customer = {};
+      $scope.earnings = {};
+    };
 
 });
