274f1eeb725ee40dfd21063dda7625ddb801682f
J Law <justin.eric.law@gmail.com>
2015-06-07 23:43:40 -0400

    Revise form layout, begin Angular markup

--- js/myApp.js
+++ js/myApp.js
@@ -1,4 +1,42 @@
 angular.module('myApp', [])
   .controller('myController', function($scope){
 
+    $scope.submit = function() {
+      if($scope.myForm.$valid) {
+        console.log('The Form is Valid');
+        compute();
+      } else {
+        console.log('The Form is Not Valid');
+      }
+    };
+
+    // Why do I need to do this?
+    $scope.customer_subtotal = 0;
+    $scope.customer_tip = 0;
+    $scope.customer_total = 0;
+
+
+    var compute = function () {
+
+      // Customer Charges
+      $scope.customer_subtotal = function () {
+        return $scope.meal_price + ($scope.meal_price * $scope.meal_tax/100);
+      };
+      $scope.customer_tip = function () {
+        return $scope.customer_subtotal() * ($scope.meal_tip/100);
+      };
+      $scope.customer_total = function () {
+        return $scope.customer_subtotal() + $scope.customer_tip();
+      };
+
+      // Earnings Info
+        $scope.tip_total += $scope.customer_tip();
+        $scope.meal_count++;
+        $scope.avg_tip = $scope.tip_total / $scope.meal_count;
+    };
+
+
+    // Reset All Data
+
+
 });
